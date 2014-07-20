8ed58e88bedadf596d2217ff66cc207fe574ea6d
Ende! <ende1337@gmail.com>
2014-07-20 13:51:41 +0200

    improved overall UI look and fixed problems with bad contrasts several improvements in stylesheet.css refactored idaskins.cpp added two custom icons updated version to 1.1.0

--- idaskins.cpp
+++ idaskins.cpp
@@ -23,10 +23,12 @@
  */
 
 #include <QtGui>
+#include <QDockWidget>
 #include <ida.hpp>
 #include <idp.hpp>
 #include <loader.hpp>
 #include <diskio.hpp>
+#include <functional>
 
 // ============================================================================
 
@@ -34,62 +36,103 @@
  * @brief   Applies the Qt stylesheet.
  * @return  true if it succeeds, false if it fails.
  */
-bool apply_stylesheet(void)
+bool apply_stylesheet()
 {
-	QString idaDir = QDir(idadir(nullptr)).absolutePath();
-	QFile stylesheet(idaDir + "/skin/stylesheet.css");
-	if (!stylesheet.open(QFile::ReadOnly))
-	{
-		msg("[IDASkins] Unable to load stylesheet file.\n");
-		return false;
-	}
+    QString idaDir = QDir(idadir(nullptr)).absolutePath();
+    QFile stylesheet(idaDir + "/skin/stylesheet.css");
+    if (!stylesheet.open(QFile::ReadOnly))
+    {
+        msg("[IDASkins] Unable to load stylesheet file.\n");
+        return false;
+    }
 
-	QString data = stylesheet.readAll();
-	data.replace("<IDADIR>", idaDir);
-	data.replace("<SKINDIR>", idaDir + "/skin");
-	qApp->setStyleSheet(data);
-	msg("[IDASkins] Skin file successfully applied! "
-		"(Reload using CTRL+SHIFT+S)\n");
-	return true;
+    QString data = stylesheet.readAll();
+    data.replace("<IDADIR>", idaDir);
+    data.replace("<SKINDIR>", idaDir + "/skin");
+    qApp->setStyleSheet(data);
+    msg("[IDASkins] Skin file successfully applied! "
+        "(Reload using CTRL+SHIFT+S)\n");
+
+    /*
+    static bool first = true;
+    // Information gathering
+    if (!first)
+    {
+        QFile log(idaDir + "/skin/object_log.log");
+        log.open(QFile::WriteOnly);
+        std::function<void(QWidget*, int)> helper = [&](QWidget *element, int depth)
+        {
+            for (int i = 0; i < depth; ++i)
+                log.write("--");
+            log.write(element->metaObject()->className());
+            log.write(" name: ");
+            log.write(element->objectName().toAscii().data());
+            if (strcmp(element->metaObject()->className(), "QLabel") == 0)
+            {
+                log.write("; text: ");
+                log.write(((QLabel*)element)->text().toAscii().data());
+            }
+            if (strcmp(element->metaObject()->className(), "QAbstractButton") == 0)
+            {
+                log.write("; icon: ");
+                log.write(((QAbstractButton*)element)->icon().name().toAscii().data());
+            }
+            log.write("; stylesheet: ");
+            log.write(element->styleSheet().toAscii().data());
+            log.write("\n");
+            auto children = element->findChildren<QWidget*>(QString());
+            for (auto it = children.begin(); it != children.end(); ++it)
+                helper(*it, depth + 1);
+        };
+        helper(qApp->activeWindow(), 0);
+        log.flush();
+        log.close();
+    }
+    first = false;
+    */
+
+    return true;
 }
 
-//-----------------------------------------------------------------------------
-
-int idaapi init(void)
+/**
+ * @brief   Initialization callback for IDA.
+ * @return  A @c PLUGIN_ constant from loader.hpp.
+ */
+int idaapi init()
 {
-	if (!is_idaq()) return PLUGIN_SKIP;
-	msg("IDASkins 1.0.0 by athre0z/Ende! loaded!\n");
-	apply_stylesheet();
-	return PLUGIN_KEEP;
+    if (!is_idaq()) return PLUGIN_SKIP;
+    msg("IDASkins 1.1.0 by athre0z/Ende! loaded!\n");
+    apply_stylesheet();
+    return PLUGIN_KEEP;
 }
 
-//-----------------------------------------------------------------------------
-
-void idaapi term(void)
+/**
+ * @brief   Shutdown callback for IDA.
+ */
+void idaapi term()
 {
-	
+    
 }
 
-//-----------------------------------------------------------------------------
-
+/**
+ * @brief   Run callback for IDA.
+ */
 void idaapi run(int /*arg*/)
 {
-	apply_stylesheet();
+    apply_stylesheet();
 }
-
-//-----------------------------------------------------------------------------
 
 plugin_t PLUGIN =
 {
-	IDP_INTERFACE_VERSION,
-	0,
-	init,
-	term,
-	run,
-	"Advanced IDA skinning",
-	"Plugin providing advanced skinning facilities using Qt stylesheets.",
-	"IDASkins: Reload stylesheet",
-	"Ctrl-Shift-S"
+    IDP_INTERFACE_VERSION,
+    0,
+    init,
+    term,
+    run,
+    "Advanced IDA skinning",
+    "Plugin providing advanced skinning facilities using Qt stylesheets.",
+    "IDASkins: Reload stylesheet",
+    "Ctrl-Shift-S"
 };
 
 // ============================================================================
